630231ad65cad08e27f5b5720250509d78221fd4
Taeil08 <ejrrhcl56@naver.com>
2025-06-27 21:51:51 +0900

    Footer 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
+      <Footer />
     </BrowserRouter>
   );
 }

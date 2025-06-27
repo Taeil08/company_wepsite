27b8d9c4ab47d9456249863c8928863f85f92978
Taeil08 <ejrrhcl56@naver.com>
2025-06-27 13:16:26 +0900

    디벨롭 브랜치 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline ">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
 }
 
 export default App;
